7ad33da8c26d5f6e96942e475c0dec079de0a159
Anastasiia <30463278+Anastat@users.noreply.github.com>
2019-02-03 15:21:49 +0200

    week 3/ 01 TODO 2

--- week 3/01/logic.js
+++ week 3/01/logic.js
@@ -4,23 +4,48 @@ window.onload = function () {
         data: {
             x: 0,
             y: 0,
-            appResult: 0
+            appResult: 0,
+            previous: 0
         },
         methods: {
             multiplyBy: function () {
                 this.appResult = this.x * this.y;
+                localStorage.sign = '*';
+                localStorage.appResult = this.appResult;
                 return this.appResult;
             },
             divideBy: function () {
                 this.appResult = this.x / this.y;
+                localStorage.sign = '/';
+                localStorage.appResult = this.appResult;
                 return this.appResult;
-            }
-        },
+            },
 
+
+        },
+        watch: {
+           x(newX) {
+            localStorage.x = newX;
+           },
+           y(newY) {
+            localStorage.y = newY;
+           },
+           previous(newPrev) {
+               this.previous = newPrev;
+               return this.previous;
+           }
+        },
         computed: {
             result: function () {
+                console.log(localStorage.getItem('x'))
                 return this.appResult;
+            },
+            prev: function () {
+                this.previous = localStorage.getItem('x') + localStorage.getItem('sign') +
+                localStorage.getItem('y') + '=' + localStorage.getItem('appResult');
+                return this.previous;
             }
+
         }
     })
 }
